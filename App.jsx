7a88dd42cda8bf5561646c677267c3c897470c5d
anilkumarsde <anilkumarlks16@gmail.com>
2024-09-20 20:47:09 +0530

    updated

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import First from './component/First'
+
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <First />
     </View>
   )
 }
