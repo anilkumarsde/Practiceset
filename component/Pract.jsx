785b9bd79be57426329256e42a3659cd9f021758
anilkumarsde <anilkumarlks16@gmail.com>
2024-11-08 19:53:18 +0530

    updated

--- component/Pract.jsx
+++ component/Pract.jsx
@@ -1,25 +1,29 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
 const Pract = () => {
-    const [text,setText]=useState('')
-    const arr = ['apple', 'orange', 'litchi', 'mango','angur','Apple']
-    let arr1 = arr.filter((fruit) => fruit.startsWith(text))
+    const [text, setText] = useState(null)
+    const arr = ['apple', 'orange', 'litchi', 'mango', 'angur', 'Apple', 'vv', 'aaa', 'hhhhhhhhh']
+    let arr1 = arr.filter((fruit) => fruit.length <= text)
     return (
-        <View style={{flex:1,
-            justifyContent:'center',
-            alignItems:'center'
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
         }}>
             <TextInput placeholder='Enter character....'
-            value={text}
-            onChangeText={(tex)=>setText(tex)}
-            style={{height:50,
-                width:100,
-                borderWidth:1,
-                justifyContent:'center',
-                alignItems:'center'
-            }} />
+                value={text}
+                keyboardType='numeric'
+                onChangeText={(tex) => setText(tex)}
+                style={{
+                    height: 50,
+                    width: 100,
+                    borderWidth: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }} />
+
             {
-                arr1.map((f, id) => <View key={id}><Text style={{fontSize:20}}>{f}</Text></View>)
+                arr1.map((f, id) => <View key={id}><Text style={{ fontSize: 20 }}>{f}</Text></View>)
             }
         </View>
     )
