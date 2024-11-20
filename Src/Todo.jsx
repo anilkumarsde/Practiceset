f59215422c60cae49ef7d45c8703abfdfb739d99
anilkumarsde <anilkumarlks16@gmail.com>
2024-11-20 14:00:14 +0530

    updated

--- Src/Todo.jsx
+++ Src/Todo.jsx
@@ -3,8 +3,17 @@ import React, { useState } from 'react'
 
 const Todo = () => {
     const [array, setArray] = useState([])
+
     const Additem = () => {
         setArray([...array, array.length + 1]);
+    }
+    const removeItem = (idx) => {
+        let newArry = array.filter((val, id) => {
+            return idx != id;
+        })
+        setArray(newArry);
+
+
     }
     return (
         <View style={styles.container}>
@@ -12,9 +21,10 @@ const Todo = () => {
                 <Text>Add item</Text>
             </TouchableOpacity>
             {
-                array.map((val, idx) => <View key={idx}>
+                array.map((val, idx) => <TouchableOpacity key={idx} style={styles.list} onPress={() => removeItem(idx)}>
                     <Text>{val}</Text>
-                </View>)
+
+                </TouchableOpacity>)
             }
         </View>
     )
@@ -25,8 +35,8 @@ export default Todo
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        // justifyContent: 'center',
-        // alignItems: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     button: {
         height: '5%',
@@ -37,5 +47,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginVertical: 10
 
+    },
+    list: {
+        backgroundColor: 'tomato',
+        marginVertical: 10,
+        width: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5
     }
 })
